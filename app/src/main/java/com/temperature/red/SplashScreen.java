a71720bc0952e2f56c7e8a15ca3850e501aea6fa
shailendra <shailendra.rao@wibmo.com>
2022-06-11 20:14:58 +0530

    Update code Splash screen

--- app/src/main/java/com/temperature/red/SplashScreen.java
+++ app/src/main/java/com/temperature/red/SplashScreen.java
@@ -1,101 +1,58 @@
 package com.temperature.red;
+
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Window;
-import android.view.WindowManager;
-import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.ImageView;
+import android.widget.TextView;
 
-import static android.content.ContentValues.TAG;
-
-/**
- * Created by Shailendra on 3/30/2021.
- */
 public class SplashScreen extends Activity {
-
     private static final String PREFS_USER_PREF = "TimeOut";
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        // TODO Auto-generated method stub
+    /* access modifiers changed from: protected */
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // remove title
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        requestWindowFeature(1);
+        getWindow().setFlags(1024, 1024);
         setContentView(R.layout.splash);
-        // HERE WE ARE TAKING THE REFERENCE OF OUR IMAGE
-        // SO THAT WE CAN PERFORM ANIMATION USING THAT IMAGE
-        ImageView backgroundImage= (ImageView) findViewById(R.id.imageLogo);
-        Animation slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide);
-        backgroundImage.startAnimation(slideAnimation);
-        Thread timerThread = new Thread(){
-            public void run(){
-                try{
+        TextView textView = (TextView) findViewById(R.id.textView3);
+        ((ImageView) findViewById(R.id.imageView3)).startAnimation(AnimationUtils.loadAnimation(this, R.anim.side_slide));
+        new Thread() {
+            public void run() {
+                Intent intent;
+                try {
                     sleep(3000);
-                }catch(InterruptedException e){
+                    intent = new Intent(SplashScreen.this, DeviceList.class);
+                } catch (InterruptedException e) {
                     e.printStackTrace();
-                }finally{
-                    if(getCounter(SplashScreen.this)<20) {
-                        setCounter(SplashScreen.this, getCounter(SplashScreen.this)+1);
-                        Intent intent = new Intent(SplashScreen.this, DeviceList.class);
-                        startActivity(intent);
-                    }else
-                        finish();
-
+                    intent = new Intent(SplashScreen.this, DeviceList.class);
+                } catch (Throwable th) {
+                    SplashScreen.this.startActivity(new Intent(SplashScreen.this, DeviceList.class));
+                    throw th;
                 }
+                SplashScreen.this.startActivity(intent);
             }
-        };
-        timerThread.start();
+        }.start();
     }
 
-    @Override
-    protected void onPause() {
-        // TODO Auto-generated method stub
+    /* access modifiers changed from: protected */
+    public void onPause() {
         super.onPause();
         finish();
     }
 
     public static int getCounter(Context context) {
-        SharedPreferences pref = context.getSharedPreferences(PREFS_USER_PREF, Context.MODE_PRIVATE);
-
-        return pref.getInt("Timelapse", 0);
+        return context.getSharedPreferences(PREFS_USER_PREF, 0).getInt("Timelapse", 0);
     }
 
     public static void setCounter(Context context, int pos) {
-        Log.d(TAG, "Timelapse: "+pos);
-        SharedPreferences pref = context.getSharedPreferences(PREFS_USER_PREF, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = pref.edit();
+        Log.d("ContentValues", "Timelapse: " + pos);
+        SharedPreferences.Editor editor = context.getSharedPreferences(PREFS_USER_PREF, 0).edit();
         editor.putInt("Timelapse", pos);
         editor.commit();
     }
-
-    /*private void Slide(){
-        // This is used to hide the status bar and make
-        // the splash screen as a full screen activity.
-        window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-        );
-
-        // HERE WE ARE TAKING THE REFERENCE OF OUR IMAGE
-        // SO THAT WE CAN PERFORM ANIMATION USING THAT IMAGE
-        TextView backgroundImage= (TextView) findViewById(R.id.textView3);
-        Animation slideAnimation = AnimationUtils.loadAnimation(this, R.anim.side_slide);
-        backgroundImage.startAnimation(slideAnimation);
-
-        // we used the postDelayed(Runnable, time) method
-        // to send a message with a delayed time.
-        Handler han = new Handler().postDelayed({
-                val intent = Intent(this, MainActivity::class.java)
-        startActivity(intent);
-        finish();
-        }, 3000); // 3000 is the delayed time in milliseconds.
-    }*/
-
 }
